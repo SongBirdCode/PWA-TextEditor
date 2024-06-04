facdda75df6f5d8b67d0a606279706d63a4e9a1a
SongBirdCode <Songbirdcode@gmail.com>
2024-06-04 04:04:21 -0400

    Fixed server

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,5 +4,5 @@ const path = require('path');
 
 module.exports = (app) =>
   app.get('/'), (req, res) =>
-    res.sendFile(path.join(__dirname,("../public/index.html")
-  );
+    res.sendFile(path.join(__dirname,"../public/index.html")
+);
