f2edfcc717a7fe1c8856e41607546358ddb2090c
SongBirdCode <Songbirdcode@gmail.com>
2024-06-04 16:47:13 -0400

    fixed starter code issues

--- routes/htmlRoutes.js
+++
@@ -1,8 +0,0 @@
-// set const for path to connect to index.html//
-
-const path = require('path');
-
-module.exports = (app) =>
-  app.get('/'), (req, res) =>
-    res.sendFile(path.join(__dirname,"../public/index.html")
-);
