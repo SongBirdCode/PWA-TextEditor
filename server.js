2213514a83f1b6d3033e6c40e894222cc65af764
SongBirdCode <Songbirdcode@gmail.com>
2024-06-11 16:52:38 -0400

    adjusted route: issue still occurs

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
-
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.get("/index.html"), (req, res) => {
+    res.send("");
+
+}
 
 app.use(express.static('./index.html'));
 app.use(express.urlencoded({ extended: true }));
