c5ece2cc3875402582d253a33f9ef3295686d2fb
SongBirdCode <Songbirdcode@gmail.com>
2024-06-11 23:52:16 -0400

    added gitignore and fix database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,19 +14,19 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  const jateDb=await openDB ("jate", 1);
-  const transaction= jateDb.transaction("jate", "readwrite");
-  const store= transaction.objectStore("jate");
+  const jateDb = await openDB ("jate", 1);
+  const transaction = jateDb.transaction("jate", "readwrite");
+  const store = transaction.objectStore("jate");
 
-  const request= store.put({id:1, value:content });
-  const results= await request;
+  const request = store.put({id:1, value:content });
+  const results = await request;
   console.log("Data has been saved to the database", results.value)
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => { 
-  const jateDb=await openDB ("jate", 1);
-  const transaction= jateDB.transaction("jate", "readonly");
+  const jateDb = await openDB ("jate", 1);
+  const transaction= jateDb.transaction("jate", "readonly");
   const store= transaction.objectStore("jate");
 
   const request= store.get(1);
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -54,7 +54,7 @@ module.exports = () => {
       rules: [
         {
           test: /\.css$/i,
-          use: ["style.loader","css-loader"]
+          use: ["style-loader","css-loader"]
 
         },
         {
